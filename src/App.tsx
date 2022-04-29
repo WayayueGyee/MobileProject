668dae1c5c814420601daafeb9df3f037c3cd188
andronzi <andrey.radioniv.2003@gmail.com>
2022-04-29 18:04:41 +0700

    add useStyles

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import {ScrollView} from "react-native";
 import { BlocksList } from './Components/BlocksList';
 
-
 const App: React.FC = () => {
   return (
     <ScrollView>
--- src/Components/InpBlock.tsx
+++ src/Components/InpBlock.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
-import {StyleSheet, TextInput, View} from "react-native";
+import { TextInput, View} from "react-native";
 import { StateAPI } from '../DataController/blockStateAPI';
+import { makeStyles, useTheme } from '@rneui/themed';
 
 export type Props = {
     value: string | undefined;
@@ -12,6 +13,8 @@ export const InpBlock: React.FC<Props> = ({
     keys
 }) => {
     const [inputValue, setInputValue] = useState(value);
+    const { theme } = useTheme();
+    const styles = useStyles(theme);
 
     const changeText = (text: string) => {
         setInputValue(text);
@@ -27,13 +30,13 @@ export const InpBlock: React.FC<Props> = ({
     )
 }
 
-const styles = StyleSheet.create({
+const useStyles = makeStyles((theme) => ({
     input: {
-        backgroundColor: 'white',
-        color: 'black',
+        background: theme.colors?.white,
+        color: theme.colors?.black,
         marginLeft: 10,
         marginRight: 10,
         minWidth: 50,
         borderRadius: 10
     }
-})
+}))
